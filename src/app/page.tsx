5a9e46277819851e3332f72adfe800962691fb24
Rishi Khan <rishi.rkhan@gmail.com>
2025-02-14 17:11:51 -0500

    feat(package.json): update next version to 15.1.7 and add react-switch dependency feat(package.json): add @eslint/eslintrc as a dev dependency feat(public): add night.svg and sun.svg icons refactor(globals.css): remove globals.css file refactor(layout.tsx): update import path for globals.css to styles/globals.css
    style(page.module.css): refactor styles in page.module.css for better organization and readability
    feat(page.tsx): add theme switch functionality to toggle between light and dark themes
    feat(globals.css): add global CSS variables for colors and theme handling
    feat(reset.css): add CSS reset to ensure consistent styling across browsers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import Switch from "react-switch";
+
 import Image from "next/image";
+
+// import { useTheme } from "./ThemeContext";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // const { theme, toggleTheme } = useTheme();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [checked, setChecked] = useState(false);
+  const theme = checked ? "dark" : "light";
+
+  const handleChange = (nextChecked: boolean) => {
+    setChecked(nextChecked);
+  };
+
+  return (
+    <div className={styles.pageContainer} data-theme={theme}>
+      <Image
+        src={theme === "light" ? "/sun.svg" : "/night.svg"}
+        alt="Theme illustration"
+        width={200}
+        height={200}
+        priority
+      />
+      <h1>{theme === "light" ? "Light Theme" : "Dark Theme"}</h1>
+      <label>
+        <Switch
+          height={35}
+          handleDiameter={32}
+          width={75}
+          checked={checked}
+          onChange={handleChange}
+          boxShadow="0 1px 5px rgba(0, 0, 0, 0.6)"
+        />
+      </label>
     </div>
   );
 }
